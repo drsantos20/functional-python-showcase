11c206e1881fd414afa35b9190758de4682a65a9
drsantos20 <drsantos20@gmail.com>
2021-05-19 15:41:28 -0300

    Add wallet objects

--- shopping/user/customer.py
+++
@@ -1,8 +0,0 @@
-from shopping.user.user import User
-
-
-class Customer(User):
-    def __init__(self, joined_at=None, **kwargs):
-        super(Customer, self).__init__(**kwargs)
-        self.joined_at = joined_at
-
--- shopping/user/wallet.py
+++ shopping/user/wallet.py
@@ -1,9 +1,14 @@
+from shopping.user.user import User
 
 
-class Wallet:
-    def __init__(self, customer, funds):
-        self.customer = customer()
+class Wallet(User):
+    def __init__(self, funds=None, **kwargs):
+        super(Wallet, self).__init__(**kwargs)
         self.funds = funds
 
-    def total_funds(self) -> int:
+    def add_funds(self, amount):
+        if amount > 0:
+            self.funds += amount
+
+    def total_funds(self):
         return self.funds
